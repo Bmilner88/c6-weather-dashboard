8bdd1a31b9f809e76c28392535d05cf7081a671a
Benny623 <bmilner88@gmail.com>
2022-02-22 23:14:17 -0700

    openweather call is functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,40 @@
 let currentWeatherEl = document.querySelector('#current-weather');
 let searchFormEl = document.querySelector('#search-form')
-//let searchBtnEl = document.querySelector('#search-btn');
+let citySearchInput = document.querySelector('#city');
 
-function getCurrentWeather(event) {
+function searchSubmitHandler(event) {
     event.preventDefault();
     
+    let cityInput = citySearchInput.value.trim();
+    
+    if(cityInput) {
+        getCurrentWeather(cityInput);
+
+    }
+    else {
+        alert('Please enter a city');
+    };
 };
 
-searchFormEl.addEventListener('submit', getCurrentWeather);
+function getCurrentWeather(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4ed8c37d6621ba937986a99f2b95f865`;
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+            else {
+                alert('Error: city not found');
+            };
+        })
+        .catch(function(error) {
+            alert('Unable to connect to OpenWeather');
+        });
+};
+
+function displayWeather(city) {};
+
+searchFormEl.addEventListener('submit', searchSubmitHandler);
