a47395ce0c26c6a416c56cf0e93005b71bcbf87f
Benny623 <bmilner88@gmail.com>
2022-02-23 17:34:01 -0700

    can't figure out the date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ let searchFormEl = document.querySelector('#search-form')
 let citySearchInput = document.querySelector('#city');
 let historyListEL = document.querySelector('#history-buttons');
 
+let createDiv = document.createElement('div');
+let createH2 = document.createElement('h2');
+
 function searchSubmitHandler(event) {
     event.preventDefault();
     
@@ -16,6 +19,14 @@ function searchSubmitHandler(event) {
     };
 };
 
+function historyClickHandler(event) {
+    let city = event.target.getAttribute('data-city');
+
+    if(city) {
+        getCurrentWeather(city);
+    };
+};
+
 function getCurrentWeather(city) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a72b9c777fb2cf77143a024a443dde88`;
 
@@ -24,8 +35,7 @@ function getCurrentWeather(city) {
             if(response.ok) {
                 response.json().then(function(data) {
                     displayWeather(data);
-
-                })
+                });
             }
             else {
                 alert('Error: city not found');
@@ -36,22 +46,32 @@ function getCurrentWeather(city) {
         });
 };
 
-
 function displayWeather(city) {
-    console.log(city)
-    addHistory(city);
+        createDiv.classList = '';
+        createH2.textContent = '';
+        let temp = city.dt.toString();
+        let date = new Date(temp);
+
+        createDiv.classList = 'card mt-5';
+        createH2.textContent = `${city.name} (${date.toDateString()})`;
+
+        createDiv.appendChild(createH2);
+        currentWeatherEl.append(createDiv);
+        console.log(temp)
 };
 
-function addHistory(city) {
-    let btn = document.createElement('button');
-    //let liId = document.querySelector(`#${city.name.toLowerCase()}`);
+/* function addHistory(city) {
+    if(city.getAttribute('data-city')) {
+        let btn = document.createElement('button');
+        //let liId = document.querySelector(`#${city.name.toLowerCase()}`);
 
-    btn.textContent = city.name;
-    btn.classList = 'btn bg-B';
-    btn.setAttribute('data-city', city.name.toLowerCase())
+        btn.textContent = city.name;
+        btn.classList = 'btn btn-dark col-12 mb-3';
+        btn.setAttribute('data-city', city.name.toLowerCase())
 
-    historyListEL.appendChild(btn);
-};
-
+        historyListEL.append(btn);
+    };
+}; */
 
 searchFormEl.addEventListener('submit', searchSubmitHandler);
+historyListEL.addEventListener('click', historyClickHandler);
