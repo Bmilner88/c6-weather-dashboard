49dc78dd365d719f8f8dc3ce7e3035a7d1b697a6
Benny623 <bmilner88@gmail.com>
2022-04-25 09:08:33 -0600

    not working

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,3 +4,5 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+});
+
+
+console.log(process.env.API_KEY)
